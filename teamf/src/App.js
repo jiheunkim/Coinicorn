974fd2563576c82d1326b54d556fad61b7830006
jiheunkim <ziheun54@gmail.com>
2023-07-26 05:35:21 +0900

    화면테스트2

--- teamf/src/App.js
+++ teamf/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [menu1, menu2, menu3, menu4] = useState('메뉴1', '메뉴2', '메뉴3', '메뉴4')
+
   return (
     <div className="App">
+      <div className="black-nav">
+        <div>LikeLion</div>
+        <h1>{ menu1 }</h1>
+      </div>
       <header className="App-header">
         <img src="https://kr.object.ncloudstorage.com/moong-images/005930.png" className="App-logo" alt="logo" />
         <p>
