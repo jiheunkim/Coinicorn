10706d487837f693532da954c0175fd22e0eab67
jiheunkim <ziheun54@gmail.com>
2023-07-26 06:46:13 +0900

    화면테스트3+상단메뉴바 구성

--- teamf/src/App.js
+++ teamf/src/App.js
@@ -1,32 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { useState } from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
 
 function App() {
-
-  let [menu1, menu2, menu3, menu4] = useState('메뉴1', '메뉴2', '메뉴3', '메뉴4')
-
   return (
-    <div className="App">
-      <div className="black-nav">
-        <div>LikeLion</div>
-        <h1>{ menu1 }</h1>
-      </div>
-      <header className="App-header">
-        <img src="https://kr.object.ncloudstorage.com/moong-images/005930.png" className="App-logo" alt="logo" />
-        <p>
-          멋쟁이사자처럼 TeamF
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
