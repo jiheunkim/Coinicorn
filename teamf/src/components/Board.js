c597ac1e0250c2033551953d17e2b51e122a8968
jiheunkim <ziheun54@gmail.com>
2023-07-31 03:51:06 +0900

    데이터 가져오기 연습

---
+++ teamf/src/components/Board.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useState, useRef } from "react";
+import axios from 'axios';
+import Tr from './Tr';
+import Post from './Post';
+import Modal from './Modal';
+
+const Board = () => {
+  const [info, setInfo] = useState([]);
+  const [selected, setSelected] = useState('');
+  const [modalOn, setModalOn] = useState(false);
+
+  // 고유 값으로 사용 될 id
+  // ref 를 사용하여 변수 담기
+  const nextId = useRef(11);
+
+//더미 데이터 호출
+  useEffect(() => {
+    axios.get('file:///C:/Users/PC/Downloads/MOCK_DATA.json')
+      .then(res => setInfo(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  const handleSave = (data) => {
+    //데이터 수정하기
+    if (data.id) { //수정 데이터에는 id가 존재
+      setInfo(
+        info.map(row => data.id === row.id ? {
+          id: data.id,
+          name: data.name,
+          email: data.email,
+          phone: data.phone,
+          website: data.website,
+        } : row))
+
+    } else { //바로 추가하기
+      // 데이터 추가하기 방법1
+      // setInfo((prev) => {
+      //   return [ ...prev, {
+      //     id: nextId.current,
+      //     name: data.name,
+      //     email: data.email,
+      //     phone: data.phone,
+      //     website: data.website
+      //   }]
+      // });
+
+      //데이터 추가하기 방법2
+      setInfo(info => info.concat(
+        {
+          id: nextId.current,
+          name: data.name,
+          email: data.email,
+          phone: data.phone,
+          website: data.website
+        }
+      ))
+      nextId.current += 1;
+    }
+  }
+
+  const handleRemove = (id) => {
+    setInfo(info => info.filter(item => item.id !== id));
+  }
+
+  const handleEdit = (item) => {
+    setModalOn(true);
+    const selectedData = {
+      id: item.id,
+      name: item.name,
+      email: item.email,
+      phone: item.phone,
+      website: item.website
+    };
+    console.log(selectedData);
+    setSelected(selectedData);
+  };
+
+  const handleCancel = () => {
+    setModalOn(false);
+  }
+
+  const handleEditSubmit = (item) => {
+    console.log(item);
+    handleSave(item);
+    setModalOn(false);
+  }
+
+  return (
+    <div className="container max-w-screen-lg mx-auto">
+      <div className='text-xl font-bold mt-5 mb-3 text-center'>🔥HOT</div>
+      <table className="min-w-full table-auto text-gray-800">
+        <thead className='justify-between'>
+          <tr className='bg-gray-800'>
+            <th className="text-gray-300 px-4 py-3">Id.</th>
+            <th className="text-gray-300 px-4 py-3">Title</th>
+            <th className="text-gray-300 px-4 py-3">Email</th>
+            <th className="text-gray-300 px-4 py-3">Phone No.</th>
+            <th className="text-gray-300 px-4 py-3">Website</th>
+            <th className="text-gray-300 px-4 py-3">Edit</th>
+            <th className="text-gray-300 px-4 py-3">Delete</th>
+          </tr>
+        </thead>
+        <Tr info={info} handleRemove={handleRemove} handleEdit={handleEdit} />
+      </table>
+      <Post onSaveData={handleSave} />
+      {modalOn && <Modal selectedData={selected} handleCancel={handleCancel} 
+      handleEditSubmit={handleEditSubmit} />}
+    </div>
+  );
+};
+
+export default Board;
--- teamf/src/components/pages/News.js
+++ teamf/src/components/pages/News.js
@@ -1,25 +1,34 @@
 import '../../App.css';
 import React, { useState } from "react";
+import Lists from '../Lists';
+import Board from '../Board';
+import Footer from '../Footer';
 import axios from "axios";
 
 function News() {
-  const [text, setText] = useState("없음");
+//   const [text, setText] = useState("없음");
   
-  const clicked = () => {
-    axios
-      .post("http://192.168.135.207:8000/", {
-        params: {
-          abc: "가나다",
-        },
-      })
-      .then((response) => setText(JSON.stringify(response.data)));
-  };
+//   const clicked = () => {
+//     axios
+//       .post("http://192.168.135.207:8000/", {
+//         params: {
+//           abc: "가나다",
+//         },
+//       })
+//       .then((response) => setText(JSON.stringify(response.data)));
+//   };
 
+//   return (
+//     <div>
+//       <h1>{text}</h1>
+//       <button onClick={clicked}>클릭</button>
+//     </div>
+//   );
   return (
-    <div>
-      <h1>{text}</h1>
-      <button onClick={clicked}>클릭</button>
-    </div>
+    <>
+      <Board />
+      <Footer />
+    </>
   );
 }
 
