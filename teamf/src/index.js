028fdb8e643af6d33509f635991a3a9819e63e7b
jiheunkim <ziheun54@gmail.com>
2023-07-31 03:49:25 +0900

    tailwind css 적용

--- teamf/src/index.js
+++ teamf/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// tailwindCss 적용
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById('root')
 );
