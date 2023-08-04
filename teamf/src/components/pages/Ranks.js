8e8f2f3bfff395b37b6fa2271a5f81851b3f87f5
jiheunkim <ziheun54@gmail.com>
2023-08-04 14:15:20 +0900

    가상화폐 순위

---
+++ teamf/src/components/pages/Ranks.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+import Coins from '../Coins';
+import Footer from '../Footer';
+
+function Ranks() {
+  return (
+    <>
+      <Coins />
+      <br></br><br></br><br></br>
+      <Footer />
+    </>
+  );
+}
+
+export default Ranks;
