459c1d78a2fad6362fa311e2c3155ff3ae7f995e
jiheunkim <ziheun54@gmail.com>
2023-08-04 14:13:35 +0900

    로그인창 레이아웃

--- teamf/src/components/pages/SignUp.js
+++ teamf/src/components/pages/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../../App.css';
+import Footer from '../Footer';
 
 const SignUp = () => {
   const [text, setText] = useState("서버 테스트");
@@ -18,56 +19,113 @@ const SignUp = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // 사용자가 입력한 이메일과 비밀번호를 객체로 만듦
     const userData = {
       email: email,
       password: password,
     };
 
-    // Axios 사용하여 서버에 POST 요청 보내기
     axios
-      .post('http://192.168.135.207:8000/', userData) // POST 요청으로 변경
+      .post('http://localhost:8000/login', userData) // 서버 주소와 엔드포인트에 맞게 변경
       .then((response) => {
-        // 서버에서 받은 응답 처리
-        setText(JSON.stringify(response.data)); // 서버 테스트 성공으로 바뀌면 success
-        console.log(response.data); // 서버 응답을 콘솔에 출력
+        if (response.data.success) {
+          setText("로그인 성공"); // 로그인 성공 메시지 표시
+        } else {
+          setText("로그인 실패"); // 로그인 실패 메시지 표시
+        }
       })
       .catch((error) => {
-        // 에러 처리 하기
         console.error(error);
       });
   };
 
   return (
-    <div className='sign-up'>
-      <form onSubmit={handleSubmit}>
-        <h1>{text}</h1>
-        <br></br>
-        <label htmlFor='email'>Email </label>
-        <input
-          type='email'
-          id='email'
-          value={email}
-          onChange={handleEmailChange}
-          placeholder='Enter your email'
-          size='40'
-          style={{ width: '300px', fontSize: '1.5rem' }}
-        />
-        <br></br>
-        <label htmlFor='password'>Password </label>
-        <input
-          type='password'
-          id='password'
-          value={password}
-          onChange={handlePasswordChange}
-          placeholder='Enter your password'
-          size='40'
-          style={{ width: '300px', fontSize: '1.5rem' }}
-        />
-        <br></br>
-        <button type='submit'>Sign Up</button>
-      </form>
-    </div>
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-white">
+        <body class="h-full">
+        ```
+      */}
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <br></br><br></br><br></br>
+      <br></br><br></br><br></br>
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Coinicorn<br></br>{text}
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                이메일
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  비밀번호
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    비밀번호 찾기
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                로그인
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            회원이 아닌가요?{'   '}
+            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              회원가입하기
+            </a>
+          </p>
+        </div>
+      </div>
+      <br></br><br></br><br></br>
+      <br></br><br></br><br></br>
+      <br></br><br></br><br></br>
+      <Footer />
+    </>
   );
 };
 
