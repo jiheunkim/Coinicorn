705d259d4d626df218bbd8791cd3ca7d7f06e27f
jiheunkim <ziheun54@gmail.com>
2023-08-09 00:12:15 +0900

    뉴스 목록 페이지 버튼 추가

--- teamf/src/components/ListTd.js
+++ teamf/src/components/ListTd.js
@@ -7,7 +7,7 @@ const ListTd = ({item}) => {
         <tr className='"bg-white border-2 border-gray-200'>
             <td className='px-4 py-3'>{item.news_id}</td>
             <Link key={item.news_id} to={`/news/detail/${item.news_id}`}>
-            <td className='px-4 py-3'>{item.news_title}(뉴스제목 들어갈 자리)<br></br>{item.news_title}(뉴스통신사) | {item.view}(조회수) | {item.src}(날짜정보)</td>
+            <td className='px-4 py-3'>{item.news_title}(뉴스제목)<br></br>{item.src} | {item.view} | {item.create_date}</td>
             </Link>
             <td className={`px-4 py-3 ${item.isRightAligned ? 'right-align' : ''}`}>
                 <img
--- teamf/src/components/Lists.js
+++ teamf/src/components/Lists.js
@@ -3,6 +3,27 @@ import React, { useEffect, useState, useRef } from "react";
 import axios from 'axios';
 import ListTr from './ListTr';
 
+/**
+ * @typedef {{
+*   id: string,
+*   rank: number,
+*   name: string,
+*   symbol: string,
+*   quotes: {
+*     KRW: {
+*       price: number,
+*       market_cap: number,
+*       volume_24h: number,
+*       percent_change_24h: number,
+*       percent_change_7d: number
+*     }
+*   }
+* }} CoinsType
+*/
+
+/**
+* @returns {JSX.Element}
+*/
 const Lists = () => {
     const [info, setInfo] = useState([]);
   
@@ -12,7 +33,7 @@ const Lists = () => {
 
   //데이터 호출
   useEffect(() => {
-    axios.post('http://192.168.133.220:8000/blockchain/apitest?format=json')
+    axios.post('http://115.85.181.240:8000/blockchain/apitest')
       .then(response => {
         setInfo(response.data);
       })
@@ -20,26 +41,71 @@ const Lists = () => {
         console.error('Error fetching data:', error);
       });
   }, []);
+
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  const refreshPage = () => {
+    window.location.reload();
+  }
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?quotes=KRW")
+    .then(response => response.json())
+    .then(json => {
+      setCoins(json.slice(0, 7));
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.log(error);
+      // 에러 넘버를 확인
+      console.log(error.response.status);
+    })
+  }, [])
+
+  const { symbol, quotes } = coins;
   
-    return (
-      <div className="container max-w-screen-lg mx-auto">
+  return (
+    <div className="container max-w-screen-lg mx-auto">
         <br></br>
-        <div className='text-xl font-bold mt-5 mb-3 text-center' style={{ fontSize: '30px', textAlign: 'center', fontFamily: 'Noto Sans KR, sans-serif' }}>
-          🔥HOT NEWS🔥
-        </div>
+        {/* <div className='text-xl font-bold mt-5 mb-3 text-center' style={{ fontSize: '30px', textAlign: 'center', fontFamily: 'Noto Sans KR, sans-serif' }}>
+        🔥HOT NEWS🔥
+        </div> */}
         <br></br>
         <table className="min-w-full table-auto text-gray-800">
-          <thead className='justify-between'>
-            <tr className='bg-gray-800'>
-              <th className="text-gray-300 px-4 py-3">Id.</th>
-              <th className="text-gray-300 px-4 py-3">Title</th>
-              <th className="text-gray-300 px-4 py-3">Image</th>
+        <thead className='justify-between'>
+            <tr className='bg-white px-4 py-3'>
+            {/* 동적으로 토큰 버튼 생성 */}
+            {coins.map((coin, index) => (
+                <th key={index}>
+                    <button
+                        className="w-btn w-btn-blue"
+                        type="button"
+                        style={{
+                            background: coin.quotes.KRW.percent_change_24h >= 0
+                                ? '#F0D4D4'
+                                : 'transparent', // 변경할 배경색
+                        }}
+                    >
+                        {coin.symbol}&nbsp;
+                        {coin.quotes.KRW.percent_change_24h >= 0 ? (
+                        <span style={{ color: '#FF3333' }}>
+                            +{coin.quotes.KRW.percent_change_24h.toFixed(2)}%
+                        </span>
+                        ) : (
+                        <span style={{ color: '#0066FF' }}>
+                            {coin.quotes.KRW.percent_change_24h.toFixed(2)}%
+                        </span>
+                        )}
+                    </button>
+                </th>
+            ))}
             </tr>
-          </thead>
-          <ListTr info={info} />
+        </thead>
+        <ListTr info={info} />
         </table>
-      </div>
-    );
+    </div>
+  );
 }
 
 export default Lists;
