a0a95e650e7c65b8382e9855463de1d570d13e06
katekim512 <katekim512@konkuk.ac.kr>
2023-08-13 21:30:40 +0900

    뉴스 상세페이지 디자인 수정

--- teamf/src/components/pages/Detail.js
+++ teamf/src/components/pages/Detail.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import RightBar from "../RightBar";
 
 const Detail = () => {
   const [info, setInfo] = useState({});
@@ -20,88 +21,43 @@ const Detail = () => {
     });
   }, [id]);
 
-  const { news_title, content, summary } = news;
+  const { news_title, content, summary,thumb_url  } = news;
   const { email, name } = news;
 
+  // top10Numbers와 coins 변수를 정의
+  const top10Numbers = Array.from({ length: 10 }, (_, i) => i + 1);
+  const coins = []; // 필요한 값을 여기에 넣어야 함
+
   return (
-    <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
-      <div className="absolute inset-0 -z-10 overflow-hidden">
-        <svg
-          className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
-          aria-hidden="true"
-        >
-          <defs>
-            <pattern
-              id="e813992c-7d03-4cc4-a2bd-151760b470a0"
-              width={200}
-              height={200}
-              x="50%"
-              y={-1}
-              patternUnits="userSpaceOnUse"
-            >
-              <path d="M100 200V.5M.5 .5H200" fill="none" />
-            </pattern>
-          </defs>
-          <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-            <path
-              d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-              strokeWidth={0}
-            />
-          </svg>
-          <rect width="100%" height="100%" strokeWidth={0} fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
-        </svg>
-      </div>
-      <div className="mx-auto max-w-[1200px] grid grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
-        <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-          <div className="lg:pr-4">
-            <div className="lg:max-w-lg">
-              <p className="text-base font-semibold leading-7 text-indigo-600">2023.08.01 14:59:00 (날짜)</p>
-              <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                {news_id}{news_title}</h1>
-              <p className="mt-6 text-xl leading-8 text-gray-700">
-              {summary}
-              </p>
-            </div>
-          </div>
+    <div className="container mx-auto px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">{news.create_date}</p>
+          <h1 className="mt-4 text-3xl font-bold text-gray-900">{news_title}</h1>
         </div>
-        <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-          {/* <img
-            className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-            src="https://posri.re.kr/images/file_thumbnail_image/63/15530/63_15530_file_thumbnail_image_1544594702.jpg"
-            alt=""
-          /> */}
+
+        <div className="mt-8 text-center">
+          <img
+            className="w-full max-w-lg mx-auto rounded-xl"
+            src={thumb_url}
+            alt="News Thumbnail"
+          />
+          
         </div>
-        <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-          <div className="lg:pr-4">
-            <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-              <p>
-                {content}
-              </p>
-              <ul role="list" className="mt-8 space-y-8 text-gray-600">
-                <li className="flex gap-x-3">
-                  <CloudArrowUpIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
-                  <span>
-                    <strong className="font-semibold text-gray-900">ex</strong> 예시
-                  </span>
-                </li>
-                <li className="flex gap-x-3">
-                  <LockClosedIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
-                  <span>
-                    <strong className="font-semibold text-gray-900">ex</strong> 예시
-                  </span>
-                </li>
+        <div className="mt-8">
+          <p className="text-lg text-gray-700">{summary}</p>
+          <p className="mt-8 text-lg text-gray-700">{content}</p>
+          <ul className="mt-8 space-y-4 text-gray-600">
+            {/* ... */}
+
               </ul>
-              <p className="mt-8">
-                내용2
-              </p>
-              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
-              <p className="mt-6">
-                내용3
-              </p>
-            </div>
-          </div>
+
+          <p className="mt-8 text-lg text-gray-700">Additional Content</p>
+          <h2 className="mt-4 text-2xl font-bold text-gray-900">No server? No problem.</h2>
+          <p className="mt-2 text-gray-700">More Content</p>
         </div>
       </div>
+      <RightBar top10Numbers={top10Numbers} coins={coins} /> {/* 변수를 전달 */}
     </div>
   );
 };
